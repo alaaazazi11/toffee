f2d31b241185106a4d96bdfbdd91c652ca9f91cc
alaaazazi11 <102272034+alaaazazi11@users.noreply.github.com>
2023-05-13 01:35:51 +0300

    catalog
    view all items

---
+++ Catalog.java
@@ -0,0 +1,22 @@
+package com.mycompany.toffe;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog {
+    private List<Item> items;
+    public int i=0;
+    public Catalog() {
+        items = new ArrayList<Item>();
+    }
+    
+    public void addItem(Item item) {
+        items.add(item);
+    }
+    
+    public void display() {
+        for (Item item : items) {
+            i++;
+            System.out.println(i+") "+item.getName() + " - " + item.getPrice() + " - " + item.getCategory() + " - " + item.getDescription() + " - " + item.getBrand() + " - " + item.getUnitType() + " - " + item.getDiscount());
+        }
+    }
+}
